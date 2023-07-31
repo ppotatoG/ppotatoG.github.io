198b8f407ec09d607901bca30181f764f325d536
ppotatoG <thgus2270@gmail.com>
2023-07-31 16:17:16 +0900

    Feat: img 100% width on mobile

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,51 +1,51 @@
 import React from "react"
 
-import {Link, graphql} from "gatsby";
+import { Link, graphql } from "gatsby"
 
-import {MDXProvider} from '@mdx-js/react';
-import {MDXRenderer} from 'gatsby-plugin-mdx'
+import { MDXProvider } from "@mdx-js/react"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import styled from "styled-components"
+import PropTypes from "prop-types"
 
-import Layout from "../components/layout";
-import CodeBlock from './CodeBlock';
+import Layout from "../components/layout"
+import CodeBlock from "./CodeBlock"
 
-const PostTemplate = ({data}) => {
-    const {title, date, author} = data.mdx.frontmatter;
-    const {body} = data.mdx;
+const PostTemplate = ({ data }) => {
+  const { title, date, author } = data.mdx.frontmatter
+  const { body } = data.mdx
 
-    return (
-        <Layout>
-            <PostTemplateStyles>
-                <hr style={{marginTop: 0}} className="separator separator__large"/>
-                <Link className="btn" to="/">
-                    Back to all posts
-                </Link>
-                <hr className="separator"/>
-                <h1>{title}</h1>
-                <h2>
-                    <span>Written by {author}</span> & Posted on <span>{date}</span>
-                </h2>
+  return (
+    <Layout>
+      <PostTemplateStyles>
+        <hr style={{ marginTop: 0 }} className="separator separator__large" />
+        <Link className="btn" to="/">
+          Back to all posts
+        </Link>
+        <hr className="separator" />
+        <h1>{title}</h1>
+        <h2>
+          <span>Written by {author}</span> & Posted on <span>{date}</span>
+        </h2>
 
-                <div className="post__body">
-                    <MDXProvider
-                        components={{
-                            pre: CodeBlock,
-                        }}
-                    >
-                        <MDXRenderer>{body}</MDXRenderer>
-                    </MDXProvider>
-                </div>
-                <hr className="separator"/>
-                <h2>
-                    Posted on <span>{date}</span>
-                </h2>
-                <hr className="separator separator__large"/>
-            </PostTemplateStyles>
-        </Layout>
-    )
-};
+        <div className="post__body">
+          <MDXProvider
+            components={{
+              pre: CodeBlock,
+            }}
+          >
+            <MDXRenderer>{body}</MDXRenderer>
+          </MDXProvider>
+        </div>
+        <hr className="separator" />
+        <h2>
+          Posted on <span>{date}</span>
+        </h2>
+        <hr className="separator separator__large" />
+      </PostTemplateStyles>
+    </Layout>
+  )
+}
 
 const PostTemplateStyles = styled.section`
   h1,
@@ -59,7 +59,7 @@ const PostTemplateStyles = styled.section`
     margin-bottom: 2.5rem;
     code {
       color: rgb(64, 63, 83);
-      font-family: Heebo,Arial,Helvetica,'Helvetica Neue',sans-serif;
+      font-family: Heebo, Arial, Helvetica, "Helvetica Neue", sans-serif;
       background: #fbfcfd;
       padding: 2px;
     }
@@ -71,18 +71,25 @@ const PostTemplateStyles = styled.section`
     }
     pre {
       color: rgb(64, 63, 83);
-      font-family: Heebo,Arial,Helvetica,'Helvetica Neue',sans-serif;
+      font-family: Heebo, Arial, Helvetica, "Helvetica Neue", sans-serif;
       background: var(--highlight-pink);
       padding: 15px;
       code {
         padding: 0;
       }
       overflow: auto;
+    }
+    a {
+      color: #222;
+    }
+
+    @media (max-width: 768px) {
+      img {
+        width: 100%;
+      }
+    }
   }
-  a {
-    color: #222;
-  }
-`;
+`
 
 export const query = graphql`
   query getPost($slug: String!) {
@@ -102,12 +109,12 @@ export const query = graphql`
       body
     }
   }
-`;
+`
 
-export default PostTemplate;
+export default PostTemplate
 
 PostTemplate.propTypes = {
-    title: PropTypes.string,
-    date: PropTypes.string,
-    author: PropTypes.string
-};
+  title: PropTypes.string,
+  date: PropTypes.string,
+  author: PropTypes.string,
+}
